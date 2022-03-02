cb13d564ce48e06f0d87f12d8405f3f3214053c0
Ross-Martin5 <rmartin.ross5@gmail.com>
2022-03-01 22:58:07 -0600

    Battle File

--- Dinosaur.py
+++ Dinosaur.py
@@ -6,8 +6,3 @@ class Dinosaur:
         self.attack = enter_attack
 
 
-godzilla = Dinosaur("Godzilla", 100, 92)
-
-mothra = Dinosaur("Mothra", 100, 78)
-
-dino_taco = Dinosaur("Dino Taco", 100, 99)
